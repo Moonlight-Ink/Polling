326cf417287ccde632d8aa9834410ea1c00a4762
MoonlightShadow1 <931371894@qq.com>
2017-11-09 10:28:08 +0800

    Poll结束，开始着手添加链表存储与查询

---
+++ User/BSP/usart/bsp_usart1.h
@@ -0,0 +1,32 @@
+#ifndef __USART1_H
+#define	__USART1_H
+
+
+#include "stm32f10x.h"
+#include <stdio.h>
+
+
+
+/**************************USART参数定义********************************/
+#define             macUSART_BAUD_RATE                       115200
+
+#define             macUSARTx                                USART1
+#define             macUSART_APBxClock_FUN                   RCC_APB2PeriphClockCmd
+#define             macUSART_CLK                             RCC_APB2Periph_USART1
+#define             macUSART_GPIO_APBxClock_FUN              RCC_APB2PeriphClockCmd
+#define             macUSART_GPIO_CLK                        RCC_APB2Periph_GPIOA     
+#define             macUSART_TX_PORT                         GPIOA   
+#define             macUSART_TX_PIN                          GPIO_Pin_9
+#define             macUSART_RX_PORT                         GPIOA 
+#define             macUSART_RX_PIN                          GPIO_Pin_10
+#define             macUSART_IRQ                             USART1_IRQn
+#define             macUSART_INT_FUN                         USART1_IRQHandler
+
+void USART1_Send(uint8_t data);
+void USART1_Send_Data(volatile u32 *buf,u16 len);
+void USART1_Send_Data1(volatile u8 *buf,u8 len);
+void                USARTx_Config                           ( void );
+extern  void NVIC_Configuration(void);
+
+
+#endif /* __USART1_H */
---
+++ User/stm32f10x_it.c
@@ -0,0 +1,205 @@
+/**
+  ******************************************************************************
+  * @file    Project/STM32F10x_StdPeriph_Template/stm32f10x_it.c 
+  * @author  MCD Application Team
+  * @version V3.5.0
+  * @date    08-April-2011
+  * @brief   Main Interrupt Service Routines.
+  *          This file provides template for all exceptions handler and 
+  *          peripherals interrupt service routine.
+  ******************************************************************************
+  * @attention
+  *
+  * THE PRESENT FIRMWARE WHICH IS FOR GUIDANCE ONLY AIMS AT PROVIDING CUSTOMERS
+  * WITH CODING INFORMATION REGARDING THEIR PRODUCTS IN ORDER FOR THEM TO SAVE
+  * TIME. AS A RESULT, STMICROELECTRONICS SHALL NOT BE HELD LIABLE FOR ANY
+  * DIRECT, INDIRECT OR CONSEQUENTI
+  
+  AL DAMAGES WITH RESPECT TO ANY CLAIMS ARISING
+  * FROM THE CONTENT OF SUCH FIRMWARE AND/OR THE USE MADE BY CUSTOMERS OF THE
+  * CODING INFORMATION CONTAINED HEREIN IN CONNECTION WITH THEIR PRODUCTS.
+  *
+  * <h2><center>&copy; COPYRIGHT 2011 STMicroelectronics</center></h2>
+  ******************************************************************************
+  */
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32f10x_it.h"
+#include <includes.h>
+#include "bsp_usart1.h"
+//#include "bsp_exti.h"
+
+
+
+extern  uint8_t   Mem_Flag;
+extern  uint8_t   Rx_Cnt;
+extern  OS_MEM    Mem;
+extern  uint32_t *Mem_blk;
+
+
+
+/** @addtogroup STM32F10x_StdPeriph_Template
+  * @{
+  */
+
+/* Private typedef -----------------------------------------------------------*/
+/* Private define ------------------------------------------------------------*/
+/* Private macro -------------------------------------------------------------*/
+/* Private variables ---------------------------------------------------------*/
+/* Private function prototypes -----------------------------------------------*/
+/* Private functions ---------------------------------------------------------*/
+
+/******************************************************************************/
+/*            Cortex-M3 Processor Exceptions Handlers                         */
+/******************************************************************************/
+
+/**
+  * @brief  This function handles NMI exception.
+  * @param  None
+  * @retval None
+  */
+void NMI_Handler(void)
+{
+}
+
+/**
+  * @brief  This function handles Hard Fault exception.
+  * @param  None
+  * @retval None
+  */
+void HardFault_Handler(void)
+{
+  /* Go to infinite loop when Hard Fault exception occurs */
+  while (1)
+  {
+  }
+}
+
+/**
+  * @brief  This function handles Memory Manage exception.
+  * @param  None
+  * @retval None
+  */
+void MemManage_Handler(void)
+{
+  /* Go to infinite loop when Memory Manage exception occurs */
+  while (1)
+  {
+  }
+}
+
+/**
+  * @brief  This function handles Bus Fault exception.
+  * @param  None
+  * @retval None
+  */
+void BusFault_Handler(void)
+{
+  /* Go to infinite loop when Bus Fault exception occurs */
+  while (1)
+  {
+  }
+}
+
+/**
+  * @brief  This function handles Usage Fault exception.
+  * @param  None
+  * @retval None
+  */
+void UsageFault_Handler(void)
+{
+  /* Go to infinite loop when Usage Fault exception occurs */
+  while (1)
+  {
+  }
+}
+
+/**
+  * @brief  This function handles SVCall exception.
+  * @param  None
+  * @retval None
+  */
+void SVC_Handler(void)
+{
+}
+
+/**
+  * @brief  This function handles Debug Monitor exception.
+  * @param  None
+  * @retval None
+  */
+void DebugMon_Handler(void)
+{
+}
+
+/**
+  * @brief  This function handles PendSVC exception.
+  * @param  None
+  * @retval None
+  */
+void PendSV_Handler(void)
+{
+}
+
+/**
+  * @brief  This function handles SysTick Handler.
+  * @param  None
+  * @retval None
+  */
+void SysTick_Handler(void)
+{
+}
+
+/******************************************************************************/
+/*                 STM32F10x Peripherals Interrupt Handlers                   */
+/*  Add here the Interrupt Handler for the used peripheral(s) (PPP), for the  */
+/*  available peripheral interrupt handler's name please refer to the startup */
+/*  file (startup_stm32f10x_xx.s).                                            */
+/******************************************************************************/
+
+/**
+  * @brief  This function handles PPP interrupt request.
+  * @param  None
+  * @retval None
+  */
+/*void PPP_IRQHandler(void)
+{
+}*/
+
+
+/**
+  * @brief  USART 中断服务函数
+  * @param  无
+  * @retval 无
+  */	
+void USART1_IRQHandler(void)
+{
+	OS_ERR err;
+	
+  uint8_t  ch;
+	
+	OSIntEnter(); 	                                     //进入中断
+	
+		/* 从内存分区 mem 获取一个内存块 */
+	if(!Mem_Flag)
+	{
+  	Mem_blk = OSMemGet(
+		                   (OS_MEM *)&Mem,
+	  									 (OS_ERR *)&err
+		                  );
+		Mem_Flag=1;
+	}
+		
+	if(USART_GetITStatus(USART1,USART_IT_RXNE)!=RESET )
+	{
+		ch = USART_ReceiveData(USART1);     //获取接收到的数据
+    *(Mem_blk+Rx_Cnt++)=ch;							 
+	}
+	
+	OSIntExit();	                                       //退出中断	
+}
+
+
+
+
+/******************* (C) COPYRIGHT 2011 STMicroelectronics *****END OF FILE****/
